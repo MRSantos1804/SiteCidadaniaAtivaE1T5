7ab5b8d1126cb832136f3efb5cd13e74d5491895
MRSantos1804 <marcosrodrigo.santos@gmail.com>
2025-05-02 15:09:53 -0300

    Add files via upload

---
+++ ChamadosDoAgente/scriptChamados.js
@@ -0,0 +1,12 @@
+// scriptChamados.js
+function openTab(tabId) {
+    const contents = document.querySelectorAll('.tab-content');
+    contents.forEach(content => {
+        content.classList.remove('active');
+    });
+    document.getElementById(tabId).classList.add('active');
+}
+
+function fechar() {
+    alert("Fechando o formulário.");
+}
